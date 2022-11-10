390c5c6977afa4d480f50344fc7a9703db90795b
Anderson Stiven Estrada Velasquez <anderson.velasquez@co.getcom.internal>
2022-11-10 09:33:28 -0500

    Maquetado

--- src/main.js
+++ src/main.js
@@ -1,235 +1,229 @@
 const cards = document.querySelector(".clothesCards");
 
-const inner = [
+
+const items = [
     {   
         id: 1,
         price: 14.00,
-        stock: 9,
-        type: "Hoodies",
-        img: "https://academlo-store.netlify.app/assets/img/featured1.png"
+        category: "Hoodies",
+        img: "https://academlo-store.netlify.app/assets/img/featured1.png",
+        quantity: 10,
     },
 
     {   
         id: 2,
         price: 24.00,
-        stock: 15,
-        type: "Shirts",
-        img: "https://academlo-store.netlify.app/assets/img/featured2.png"
+        category: "Shirts",
+        img: "https://academlo-store.netlify.app/assets/img/featured2.png",
+        quantity:15,
     },
 
     {   
         id: 3,
-        price: 54.00,
-        stock: 20,
-        type: "Sweartshirts",
-        img: "https://academlo-store.netlify.app/assets/img/featured3.png"
+        price: 24.00,
+        category: "Sweartshirts",
+        img: "https://academlo-store.netlify.app/assets/img/featured3.png",
+        quantity: 20,
     }
+    
 ];
 
 let str = '';
 
-inner.forEach(element => {
+items.forEach(product => {
     str += `<div class="cardFull">
-    <div class="button" data-idUser='${element.id}'>+</div>
+            <div class="button" data-idUser='${product.id}'>+</div>
                     <div class="card">
-                    <img src="${element.img}" alt="" srcset="">
-                </div>
-                <div class="cardFooter">
-                    <div class="priceContainer">
-                        <div class="price">$${element.price}.00</div>
-                        <div class="priceSeparator"></div>
-                        <div class="stock">stock: ${element.stock}</div>
+                    <img src="${product.img}" alt="" srcset="">
                     </div>
-                    <div class="typeClothes">${element.type}</div>
+                <   div class="cardFooter">
+                    <div class="priceContainer">
+                        <div class="price">$${product.price}.00</div>
+                        <div class="priceSeparator"></div>
+                        <div class="stock">stock: ${product.quantity}</div>
+                    </div>
+                    <div class="typeClothes">${product.category}</div>
                 </div>
-          </div>`;
+            </div>`;
 });
 
 cards.innerHTML = str;
 
-export {inner}
+const pusrchase = {}
+const purchaseCart = document.querySelector(".cartMainEverything");
+const totalP = document.querySelector('.alignContainer')
 
-
-
-const orderBuys = {}
-const buyCart = document.querySelector(".cartMainEverything");
-const totalV = document.querySelector('.alignContainer')
-
-let emptyCart = `<div class="imageCart">
+let cartCar = `<div class="imageCart">
      <img src="https://academlo-store.netlify.app/assets/img/empty-cart.png">
  </div> 
  <h2>You card is empaty</h2>
  `
+purchaseCart.innerHTML = cartCar
 
-buyCart.innerHTML = emptyCart
-
-let productsBuys = []
+let buyProducts = []
 
 document.addEventListener('click', function (event) {
     if (event.target.classList.contains("button")) {
         const idClothes = event.target.dataset.iduser;
 
         let currentClothes = null;
-        for (let i = 0; i < inner.length; i++) {
-            if (inner[i].id === parseInt(idClothes)) {
-                currentClothes = inner[i];
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].id === parseInt(idClothes)) {
+                currentClothes = items[i];
             }
         }
 
-        if (orderBuys[currentClothes.id]) {
-            orderBuys[currentClothes.id].amount++;
+        if (pusrchase[currentClothes.id]) {
+            pusrchase[currentClothes.id].amount++;
         } else {
-            orderBuys[currentClothes.id] = currentClothes;
-            orderBuys[currentClothes.id].amount = 1;
+            pusrchase[currentClothes.id] = currentClothes;
+            pusrchase[currentClothes.id].amount = 1;
         }
 
-        const order = Object.values(orderBuys)
+        const order = Object.values(pusrchase)
         const amount = document.querySelector('#amount');
-        amount.textContent = Object.entries(orderBuys).length;
+        amount.textContent = Object.entries(pusrchase).length;
 
         let array = '';
 
         
-        order.forEach(element => {
-            array += `<div class="buysContent">
-            <div class="imgCartBuy">
-            <img src="${element.img}" alt="">
+        order.forEach(product => {
+            array += `<div class="shopContent">
+            <div class="imgCartShop">
+            <img src="${product.img}" alt="">
             </div>
             <div class="infoClothes">
-            <div class="principalProductInfo">
-            <div class="clothesName">${element.type}</div>
-            <div class="clothesprice">Stock: ${element.stock} | $${element.price}.00</div>
-            <div class="subTotal">Subtotal: $${(parseInt(element.price) * (element.amount))}.00</div>
+            <div class="ProductInfo">
+            <div class="clothesName">${product.category}</div>
+            <div class="clothesprice">Stock: ${product.quantity} | $${product.price}.00</div>
+            <div class="subTotal">Subtotal: $${(parseInt(product.price) * (product.amount))}.00</div>
             </div>
-            <div class="buttonsCart">
-            <div data-id="${element.id}" class="operator remove">-</div>
-            <div class="quantify">${element.amount} units</div>
-            <div data-id="${element.id}" class="operator add">+</div>
-            <i data-id="${element.id}" class=" trash fi fi-rr-trash"></i>
+            <div class="btnCart">
+            <div data-id="${product.id}" class="operator remove">-</div>
+            <div class="quantify">${product.amount} units</div>
+            <div data-id="${product.id}" class="operator add">+</div>
+            <i data-id="${product.id}" class=" trash fi fi-rr-trash"></i>
             </div>
             </div>
             </div>`
         });
         
-        productsBuys = order;
+        buyProducts = order;
        
-
-
-        console.log(event)
-        buyCart.innerHTML = array
+        purchaseCart.innerHTML = array
 
     };
 
     if (event.target.classList.contains("add")) {
-        let elementFinded = productsBuys.find((elementErase) =>
+        let elementFinded = buyProducts.find((elementErase) =>
             elementErase.id == event.target.dataset.id
         )
 
         console.log(elementFinded.amount++)
         let array = '';
 
-        productsBuys.forEach(element => {
-            array += `<div class="buysContent">
-               <div class="imgCartBuy">
-               <img src="${element.img}" alt="">
+        buyProducts.forEach(product => {
+            array += `<div class="shopContent">
+               <div class="imgCartShop">
+               <img src="${product.img}" alt="">
                </div>
                <div class="infoClothes">
-                   <div class="principalProductInfo">
-                       <div class="clothesName">${element.type}</div>
-                       <div class="clothesprice">Stock: ${element.stock} | $${element.price}.00</div>
-                       <div class="subTotal">Subtotal: $${(parseInt(element.price) * (element.amount))}.00</div>
+                   <div class="ProductInfo">
+                       <div class="clothesName">${product.category}</div>
+                       <div class="clothesprice">Stock: ${product.quantity} | $${product.price}.00</div>
+                       <div class="subTotal">Subtotal: $${(parseInt(product.price) * (product.amount))}.00</div>
                    </div>
-                   <div class="buttonsCart">
-                       <div data-id="${element.id}" class="operator remove">-</div>
-                       <div class="quantify">${element.amount} units</div>
-                       <div data-id="${element.id}" class="operator add">+</div>
-                       <i data-id="${element.id}" class=" trash fi fi-rr-trash"></i>
+                   <div class="btnCart">
+                       <div data-id="${product.id}" class="operator remove">-</div>
+                       <div class="quantify">${product.amount} units</div>
+                       <div data-id="${product.id}" class="operator add">+</div>
+                       <i data-id="${product.id}" class=" trash fi fi-rr-trash"></i>
                    </div>
                </div>
            </div>`
         });
 
-        buyCart.innerHTML = array
+        purchaseCart.innerHTML = array
     }
 
     if (event.target.classList.contains("remove")) {
-        let elementFinded = productsBuys.find((elementErase) =>
+        let elementFinded = buyProducts.find((elementErase) =>
             elementErase.id == event.target.dataset.id)
 
         if (elementFinded.amount == 0) {
             console.log('hola')
-            productsBuys.splice(productsBuys.findIndex((elementErase) =>
+            buyProducts.splice(buyProducts.findIndex((elementErase) =>
                 elementErase.id == event.target.dataset.id
             ),1) }
 
             console.log(elementFinded.amount--)
         let array = '';
 
-        productsBuys.forEach(element => {
-            array += `<div class="buysContent">
-               <div class="imgCartBuy">
-               <img src="${element.img}" alt="">
+        buyProducts.forEach(product => {
+            array += `<div class="shopContent">
+               <div class="imgCartShop">
+               <img src="${product.img}" alt="">
                </div>
                <div class="infoClothes">
-                   <div class="principalProductInfo">
-                       <div class="clothesName">${element.type}</div>
-                       <div class="clothesprice">Stock: ${element.stock} | $${element.price}.00</div>
-                       <div class="subTotal">Subtotal: $${(parseInt(element.price) * (element.amount))}.00</div>
+                   <div class="ProductInfo">
+                       <div class="clothesName">${product.category}</div>
+                       <div class="clothesprice">Stock: ${product.quantity} | $${product.price}.00</div>
+                       <div class="subTotal">Subtotal: $${(parseInt(product.price) * (product.amount))}.00</div>
                    </div>
-                   <div class="buttonsCart">
-                       <div data-id="${element.id}" class="operator remove">-</div>
-                       <div class="quantify">${element.amount} units</div>
-                       <div data-id="${element.id}" class="operator add">+</div>
-                       <i data-id="${element.id}" class=" trash fi fi-rr-trash"></i>
+                   <div class="btnCart">
+                       <div data-id="${product.id}" class="operator remove">-</div>
+                       <div class="quantify">${product.amount} units</div>
+                       <div data-id="${product.id}" class="operator add">+</div>
+                       <i data-id="${product.id}" class=" trash fi fi-rr-trash"></i>
                    </div>
                </div>
            </div>`
         });
 
-        buyCart.innerHTML = array
+        purchaseCart.innerHTML = array
     }
 
     function totalValue() {
         let total = 0;
-        productsBuys.forEach((valueTotal) => total += (valueTotal.price * valueTotal.amount))
+        buyProducts.forEach((valueTotal) => total += (valueTotal.price * valueTotal.amount))
         
         let dataTotalAmount = `<div id="totalPay" class="total">$${total}</div>
         <button>Checkout</button>`
         
-        totalV.innerHTML = dataTotalAmount
+        totalP.innerHTML = dataTotalAmount
         console.log(dataTotalAmount)
     }
     
     
     if (event.target.classList.contains("trash")){
-        productsBuys.splice(productsBuys.findIndex((elementErase) =>
+        buyProducts.splice(buyProducts.findIndex((elementErase) =>
         elementErase.id == event.target.dataset.id
         ),1)
 
         let array = '';
 
-        productsBuys.forEach(element => {
-            array += `<div class="buysContent">
-               <div class="imgCartBuy">
-               <img src="${element.img}" alt="">
+        buyProducts.forEach(product => {
+            array += `<div class="shopContent">
+               <div class="imgCartShop">
+               <img src="${product.img}" alt="">
                </div>
                <div class="infoClothes">
-                   <div class="principalProductInfo">
-                       <div class="clothesName">${element.type}</div>
-                       <div class="clothesprice">Stock: ${element.stock} | $${element.price}.00</div>
-                       <div class="subTotal">Subtotal: $${(parseInt(element.price) * (element.amount))}.00</div>
+                   <div class="ProductInfo">
+                       <div class="clothesName">${product.category}</div>
+                       <div class="clothesprice">Stock: ${product.quantity} | $${product.price}.00</div>
+                       <div class="subTotal">Subtotal: $${(parseInt(product.price) * (product.amount))}.00</div>
                    </div>
-                   <div class="buttonsCart">
-                       <div data-id="${element.id}" class="operator remove">-</div>
-                       <div class="quantify">${element.amount} units</div>
-                       <div data-id="${element.id}" class="operator add">+</div>
-                       <i data-id="${element.id}" class=" trash fi fi-rr-trash"></i>
+                   <div class="btnCart">
+                       <div data-id="${product.id}" class="operator remove">-</div>
+                       <div class="quantify">${product.amount} units</div>
+                       <div data-id="${product.id}" class="operator add">+</div>
+                       <i data-id="${product.id}" class=" trash fi fi-rr-trash"></i>
                    </div>
                </div>
            </div>`
         });
 
-        buyCart.innerHTML = array
+        purchaseCart.innerHTML = array
     }
 
     totalValue();
